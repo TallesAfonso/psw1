a89e6a8eb599442da9623daa2ef4c7e8a0681f1b
Talles Afonso de Souza <146476365+TallesAfonso@users.noreply.github.com>
2024-11-13 16:49:08 -0300

    Add files via upload

---
+++ aula-13-11-24/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>aula-13-11-24</title>
+</head>
+<body>
+
+<h1>PSW É DAORA</h1>
+
+<nav>
+    <ul>
+    
+    <li><a href="https://www.fortnite.com/?lang=pt-BR">fortnaite</a></li>
+    <li><a href="https://www.rockstargames.com/br/reddeadredemption2">Red dead 2</a></li>
+    <li><a href="https://www.roblox.com/pt/home">Roblox</a></li>
+    <li><a href="https://www.minecraft.net/pt-br">mainicrefiti</a></li>
+    
+    </ul>
+</nav>
+    
+</body>
+</html>
